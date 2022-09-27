4d925b00327bfc7384d1453e7b0abeb26d962cb9
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-27 16:57:32 +0200

    da sistemare il for

--- for/script.js
+++ for/script.js
@@ -4,4 +4,13 @@ let output = document.getElementById("output");
 
 for (let i = 0; i < budget.lenght; i++) {
   console.log(budget[i]);
+  if (budget[i] == 5) {
+    output.innerHTML = "Sei a metà del tuo budget!";
+  }
+  if (budget[i] <= 2) {
+    output.innerHTML = "Sei quasi a secco.";
+  }
+  if (budget[i] == 0) {
+    output.innerHTML = "Non hai più nulla!";
+  }
 }
