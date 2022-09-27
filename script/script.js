e76fa357a85ab16cc65bec5c0692ec624eb3b30a
dimitrilzk <dimitrilazuka95@gmail.com>
2022-09-27 14:07:50 +0200

    starter pack

---
+++ script/script.js
@@ -0,0 +1,5 @@
+// creare una funzione di budget che scali progressivamente
+// creare 2 messaggi: 1 a metà budget e 2 quando il buudget sta per esaurirsi
+// entrami i messaggi devono avvisare lo stato
+// creare un output che mostri il budget
+let budget = 100;
